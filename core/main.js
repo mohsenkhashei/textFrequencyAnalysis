8597e89d2584323be523ff5ecd623e47c23b671d
MohsenKhashei <m.khasheii@gmail.com>
2023-04-24 02:53:20 +0300

    adding Electron part

--- core/main.js
+++ core/main.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const { init, processSwitchLetters } = require("./tools/index");
 
 function createWindow() {
   // Create the browser window.
@@ -17,7 +18,7 @@ function createWindow() {
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, "index.html"));
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -36,63 +37,23 @@ app.whenReady().then(() => {
   });
 
   ipcMain.on("fileData", (event, data) => {
-    console.log(data);
-    console.log("here");
-    // You can perform further operations with the file data in the main process, such as saving it to a file, processing it, etc.
+    let processedData = init(data);
+    event.sender.send("fileDataResponse", { processedData });
   });
 });
-ipcMain.on("form-submission", (event, data) => {
-  // const fileInput = document.getElementById('fileInput');
-  // const file = fileInput.files[0]; // Get the selected file
-  // if (file) {
-  //   const reader = new FileReader();
-  //   reader.onload = (e) => {
-  //     const fileData = e.target.result; // Get the file data as ArrayBuffer, DataURL, etc.
-  //     // You can use the fileData variable to perform further operations with the file data, such as sending it to the main process via IPC or displaying it in the user interface.
-  //     console.log(fileData);
-  //   };
-  //   reader.readAsText(file);
-  //   console.log("Form submitted:", data);
-  //   let dataToSend = {
-  //     text: data.text,
-  //     language: data.language,
-  //     processType: data.processType,
-  //     shiftCount: data.shiftCount,
-  //   };
-  //   const startProcess = require("./encryptOrDecrypt")(dataToSend);
-  //   console.log(startProcess);
-  //   // Here you can handle the form submission data however you like
-  //   // For this example, we'll just send back a message indicating success
-  //   let processType = dataToSend.processType == 0 ? "Encrypt" : "Decrypt";
-  //   let language = "";
-  //   switch (dataToSend.language) {
-  //     case 0:
-  //       language = "English";
-  //       break;
-  //     case 1:
-  //       language = "French";
-  //       break;
-  //     case 2:
-  //       language = "Spanish";
-  //       break;
-  //     case 3:
-  //       language = "Turkish";
-  //       break;
-  //     default:
-  //       language = "English";
-  //   }
-  //   let template = `
-  //   <div class="alert alert-info" ><p>input: ${dataToSend.shiftCount}:${dataToSend.processType}:${dataToSend.language}:${dataToSend.text}</p></div>
-  // <div class="alert alert-success" role="alert">
-  //   <h4 class="alert-heading">Done!</h4>
-  //   <p>${processType}ing the "<b>${dataToSend.text}</b>" text with shift count of <b>${dataToSend.shiftCount}</b> in <b>${language}</b> language</p>
-  //   <hr>
-  //   <p class="mb-0">${processType}ed text is: <b>${startProcess}</b></p>
-  // </div>`;
-  //   event.sender.send("form-submission-reply", { message: template });
-  // event.reply("form-submission-reply", { message: template });
-});
 // Quit when all windows are closed, except on macOS. There, it's common
+ipcMain.on("switchLetters", (event, replacementInput) => {
+  // console.log(replacementInput);
+  // const myObject = JSON.parse(serializedData);
+  processSwitchLetters(replacementInput)
+    .then((result) => {
+      console.log(result);
+      event.sender.send("switchLettersResponse", result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
--- core/renderer.js
+++ core/renderer.js
@@ -7,6 +7,27 @@
  */
 const { ipcRenderer } = require("electron");
 const fs = require("fs");
+
+function validateInput() {
+  var input = document.getElementById("repControlInput").value; // Get input value from HTML input field
+  var errorElement = document.getElementById("error"); // Get error element by its ID
+
+  // Define a regular expression pattern to match the input format
+  var pattern = /^([a-zA-Z]+:[a-zA-Z]+(,[\s]*[a-zA-Z]+:[a-zA-Z]+)*)$/;
+  // Test the input against the regular expression pattern
+  var isValid = pattern.test(input);
+
+  if (isValid) {
+    // If input is valid, hide error message (if shown)
+    errorElement.style.display = "none";
+    return true;
+  } else {
+    // If input is invalid, show error message
+    errorElement.style.display = "block";
+    return false;
+  }
+}
+
 function getFileText() {
   const fileInput = document.getElementById("fileInput");
 
@@ -24,6 +45,72 @@ function getFileText() {
 
       // Send file data to main process via IPC
       ipcRenderer.send("fileData", data);
+      ipcRenderer.on("fileDataResponse", (event, processedData) => {
+        // Access the processed data from the main process
+        const content = document.getElementById("content");
+        let paragraph = processedData.processedData.paragraph;
+        let frAnalysis = processedData.processedData.frAnalysis;
+        let strAnalysis = "";
+        for (let i = 0; i < frAnalysis.length; i++) {
+          strAnalysis +=
+            frAnalysis[i].letter + "->" + frAnalysis[i].count + " ";
+        }
+        let contentTemplate = `<p class="fw-semibold">${paragraph}</p>
+          <div class="shadow p-3 mb-2 bg-body-tertiary rounded">
+            <p class="fs-10 fst-italic">Frequency Analysis:</p>
+            <p class="fw-bold count-result">${strAnalysis}</p>
+          </div>`;
+
+        content.innerHTML += contentTemplate;
+        console.log(processedData);
+      });
     });
   }
+}
+
+function processData() {
+  if (validateInput()) {
+    const replacementInput = document.getElementById("repControlInput");
+    const pare = document.getElementById("pare");
+    console.log(replacementInput.value);
+    ipcRenderer.send("switchLetters", replacementInput.value);
+    ipcRenderer.on("switchLettersResponse", (event, result) => {
+      let frAnalysis = result.frAnalysis;
+      let strAnalysis = "";
+      for (let i = 0; i < frAnalysis.length; i++) {
+        strAnalysis += frAnalysis[i].letter + "->" + frAnalysis[i].count + " ";
+      }
+      let contentTemplate = `
+    <section class="card show-section">
+    <div class="row" id="nextContent">
+      <div class="col">
+        <div class="shadow p-3 mb-2 bg-body-tertiary rounded">
+          <p class="fs-10 fst-italic">Your previous changes is:</p>
+          <p class="fw-bold count-result">${result.replacement}</p>
+        </div>
+      </div>
+    </section>
+    <section class="card show-section">
+      <div class="row" id="nextContent">
+        <div class="col">
+          <div class="alert alert-success" role="success">
+            Here Is Your File Content.
+          </div>
+          <p class="fw-semibold">${result.paragraph}</p>
+            <div class="shadow p-3 mb-5 bg-body-tertiary rounded">
+              <p class="fs-10 fst-italic">Frequency Analysis:</p>
+              <p class="fw-bold count-result">${strAnalysis}</p>
+            </div>
+        </div>
+      </div>
+    </section>
+    `;
+      pare.innerHTML += contentTemplate;
+    });
+  }
+}
+
+function exit() {
+  let exitSection = document.getElementById("exitSection");
+  exitSection.style.display = "none";
 }
